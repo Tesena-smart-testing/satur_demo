ad73503f32f7b3251f26414d8a455814dd12a467
marina51533 <marina51533@gmail.com>
2023-07-03 11:15:47 +0200

    Created script to send data from json file to DB, added info into Readme

---
+++ RobotFramework/send_data_to_db.py
@@ -0,0 +1,48 @@
+import json
+import mysql.connector
+
+# Read the JSON file
+with open('RobotFramework/data_output.json') as file:
+    data = json.load(file)
+
+# Connect to MySQL database
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Create1234+',
+    database='Satur'
+)
+cursor = db.cursor()
+
+
+# Iterate over the data and insert into MySQL
+for item in data:
+    hotel = item['Hotel']
+    url = item['URL']
+    termin_satur = item['termin satur']
+    terminovy_posun = item['terminovy posun']
+    pocet_noci = item['pocet noci']
+    pax = item['PAX']
+    strava = item['strava']
+    odlet = item['odlet']
+    tuzemske_ck = item['tuzemske CK']
+    max_pocet_vysledkov = item['max pocet vysledkov']
+    izba = item['izba']
+    ck = item['CK']
+    termin_ck = item['termin CK']
+    cena_za_osobu = item['cena za osobu']
+    cena_za_zajezd = item['cena za zajezd']
+    timestamp = item['timestamp']
+
+    # Construct the SQL query
+    query = "INSERT INTO Satur (Hotel, URL, termin_satur, terminovy_posun, pocet_noci, PAX, strava, odlet, tuzemske_ck, max_pocet_vysledkov, izba, CK, termin_ck, cena_za_osobu, cena_za_zajezd, timestamp) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (hotel, url, termin_satur, terminovy_posun, pocet_noci, pax, strava, odlet, tuzemske_ck, max_pocet_vysledkov, izba, ck, termin_ck, cena_za_osobu, cena_za_zajezd, timestamp)
+
+    # Execute the query
+    cursor.execute(query, values)
+    db.commit()
+
+# Close the database connection
+db.close()
+
+
